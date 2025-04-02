5db9897eb7772228db218f58925dadb99ff69515
Dario <dariogaleh@outlook.com>
2025-04-02 09:34:13 +0200

    import style change

--- src/append-to-file.ts
+++ src/append-to-file.ts
@@ -1,6 +1,5 @@
-import { promises } from 'fs';
+import { appendFile } from 'node:fs/promises';
 
 export async function appendToFile(fileName: string, text: string, translation: string, voice: string): Promise<void> {
-    const { appendFile } = promises;
     await appendFile(fileName, `${text}\t${translation}\t${voice}\n`);
 }
